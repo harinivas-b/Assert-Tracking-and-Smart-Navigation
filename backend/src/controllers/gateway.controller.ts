2315ea161581df403999d4b0be8b3f33a44e23f0
Harinivas B <harinivas.b2025ece@sece.ac.in>
2026-09-23 16:07:46 +0530

    Fix Prisma imports for serverless backend

--- backend/src/controllers/gateway.controller.ts
+++ backend/src/controllers/gateway.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-import prisma from '../server';
+import { prisma } from '../server';
 import { realtimeService } from '../services/realtimeService';
 
 export const getGateways = async (req: Request, res: Response, next: NextFunction) => {
--- backend/src/services/thingspeakService.ts
+++ backend/src/services/thingspeakService.ts
@@ -1,4 +1,4 @@
-import prisma from '../server';
+import { prisma } from '../server';
 import { processRawObservation } from './locationEngine';
 
 interface ThingSpeakChannelMeta {
